4994cd5fd22c46916e69a75410be4f3413bf3f94
yrysNM <yrys.n3m@gmail.com>
2023-10-27 09:35:28 +0600

    refactor: change getPexels method for request  https.ts

--- src/api/getPexels.ts
+++ src/api/getPexels.ts
@@ -1,30 +1,30 @@
-import { request } from "@/utils/https";
+import { request } from "@/utils/https"
 
 export interface IPexels {
-  id: number;
-  width: number;
-  height: number;
-  url: string;
-  photographer: string;
-  photographer_url: string;
-  photographer_id: number;
-  avg_color: string;
+  id: number
+  width: number
+  height: number
+  url: string
+  photographer: string
+  photographer_url: string
+  photographer_id: number
+  avg_color: string
   src: {
-    original: string;
-    large2x: string;
-    large: string;
-    medium: string;
-    small: string;
-    portrait: string;
-    landscape: string;
-    tiny: string;
-  };
-  liked: boolean;
-  alt: string;
+    original: string
+    large2x: string
+    large: string
+    medium: string
+    small: string
+    portrait: string
+    landscape: string
+    tiny: string
+  }
+  liked: boolean
+  alt: string
 }
 
 export const getPexels = async (per_page: number) => {
-  const pexels = await request<IPexels, {}>(`curated?per_page=${per_page}`, {});
+  const pexels = await request<IPexels, never>(`curated?per_page=${per_page}`, null)
 
-  return pexels;
-};
+  return pexels
+}
--- src/utils/https.ts
+++ src/utils/https.ts
@@ -1,13 +1,28 @@
-export const request = <T, B>(url: string, bodyData: B, methods: string = "GET"): Promise<T> => {
+interface IRequest {
+  method: string
+  headers: {
+    "Content-Type": "application/json"
+    Authorization: string
+  }
+  body?: string
+}
+
+export const request = <T, B>(url: string, bodyData: B | null, method = "GET"): Promise<T> => {
   const baseUrl = `https://api.pexels.com/v1/${url}`
-  return fetch(baseUrl, {
+
+  const requestInfo: IRequest = {
+    method,
     headers: {
-      methods,
-      "Content-type": "application/json",
-      Authorization: process.env.API_KEY,
+      "Content-Type": "application/json",
+      Authorization: process.env.VUE_APP_API_KEY,
     },
-    body: JSON.stringify(bodyData),
-  })
+  }
+
+  if (method !== "GET") {
+    requestInfo.body = JSON.stringify(bodyData)
+  }
+
+  return fetch(baseUrl, requestInfo)
     .then((res) => res.json())
     .then((data: T) => data)
 }
